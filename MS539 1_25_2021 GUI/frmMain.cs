ae43a2302040db71fef94ad9fb3e13bc5f6cf3e6
roddeval <roddeval@uat.edu>
2021-01-23 14:27:52 -0600

    added comment block in frmMain.cs codebehind at top of the file

--- MS539 1_25_2021 GUI/frmMain.cs
+++ MS539 1_25_2021 GUI/frmMain.cs
@@ -15,6 +15,24 @@ Roderick DeValcourt 1/22/2021	MS539 1_25_2021 GUI
 Start                   Stop
 1/22/2021  8:00 PM      1/22/2021 9:00 PM
 1/23/2021 11:00 AM      1/22/2021 1:00 PM
+
+Program explanation
+design
+    three windows
+    main
+    basic input validations
+    error provider validations
+inputs
+    name length > 0
+    dob valid date time
+    gender  male=0 female = 1
+    age - integer value > 0
+validations
+    validation routine as a method not behind a window but in logical method
+architecture
+    3 windows
+    start of a full stack implementation
+
  */
 
 namespace MS539_1_25_2021_GUI
